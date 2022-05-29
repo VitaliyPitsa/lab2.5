b02d15f88e40b7b0e877c7a172f3bd444eda855f
VitaliyPitsa <vitalik.gorshkov.1913@mail.ru>
2022-05-29 16:38:52 +0300

    1commit

---
+++ individual1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+if __name__ == '__main__':
+    arr = tuple(map(int, input().split()))
+    print("кортеж:", arr)
+
+    for i in range(len(arr) - 2):
+        e1 = arr[i]
+        e2 = arr[i + 1]
+        e3 = arr[i + 2]
+        if e1 < e2 > e3:
+            print("индексы элементов кортежа:", i, i + 1, i + 2, " их значения:", e1, e2, e3)
+            break
